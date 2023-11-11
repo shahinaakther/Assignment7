4f20c589c4c799f40648ddcbe3bd76e63c0b71dd
shahinaakther <shahinamahrus@gmail.com>
2023-11-11 19:00:56 +0600

    first commit

---
+++ app/Http/Controllers/OstadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OstadController extends Controller
+{
+    //
+
+    function SayHi(){
+        return ("Welcome to our Laraval Course");
+    }
+    function Hello(){
+        return ("we are developer");
+    }
+}
